faec89a4cf0351f33397cd878d99da291ec65a12
Daan Faber <daanfaber1@gmail.com>
2023-03-06 11:22:13 +0100

    Formatted

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -6,16 +6,16 @@ module.exports = {
     once: false,
     async execute(client) {
         if (!interaction.isChatInputCommand()) return;
-    
-    const command = interaction.client.commands.get(interaction.commandName);
 
-    if (!command) return log.error(`No command matching ${interaction.commandName} was found.`);
+        const command = interaction.client.commands.get(interaction.commandName);
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        log.error(error);
-        await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true});
-    }
+        if (!command) return log.error(`No command matching ${interaction.commandName} was found.`);
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            log.error(error);
+            await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+        }
     }
 }
