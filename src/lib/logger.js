564503467e77e444baaef210e9bc9e8eb3d699f2
Daan Faber <daanfaber1@gmail.com>
2023-03-06 11:16:18 +0100

    Added fatal log level, and defined the log levels

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -4,16 +4,24 @@ function getLogString(level, message) {
 }
 
 module.exports = {
+    // Significant event, but nothing special
     info(message) {
         console.info(getLogString('INFO', message));
     },
+    // Relevant to debugging, should not be used in production
     debug(message) {
         console.debug(getLogString('DEBUG', message));
     },
+    // Something happened to influence the outcome of a command, but there was still a succesfull result to show to the user
     warn(message) {
         console.warn(getLogString('WARNING', message));
     },
+    // A non-fatal error occured, the bot is still online, but there was no output to show to the user
     error(message) {
         console.error(getLogString('ERROR', message));
+    },
+    // A fatal error occured, the bot is no longer online, and the user did not get a response to their command
+    fatal(message) {
+        console.error(getLogString('FATAL', message));
     }
 }
