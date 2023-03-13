23bac2bb66d97738d6abe50ae0f395a23f4a2ac6
Daan Faber <daanfaber1@gmail.com>
2023-03-13 10:19:27 +0100

    Rewrote logger to save logs to DB isntead of local file

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,42 +1,40 @@
 const fs = require('fs');
-const path = require('path');
+const LogModel = require('../lib/db/model/log');
 
-function log(payload) {
-    const date = new Date();
-    const UTCstring = date.toISOString().substring(0, 10);
-    const logFile = `../logs/${UTCstring}.txt`;
+async function log(level, message) {
+    const logRec = await LogModel.new(level, message);
+    if (!logRec) console.error('Failed to save log in database');
 
-    fs.appendFile(logFile, payload + '\n', 'utf8', (err) => {
-        if (err) throw err;
-    });
-
-    console.log(payload);
+    console.log(getLogString(logRec));
 }
 
-function getLogString(level, message) {
-    const dateIsoString = new Date().toISOString();
+function getLogString(logRec) {
+    const dateIsoString = logRec.createdAt.toISOString();
+    const level = logRec.level;
+    const message = logRec.message;
+
     return `${dateIsoString} | ${level} | ${message}`;
 }
 
 module.exports = {
     // Significant event, but nothing special
     info(message) {
-        log(getLogString('INFO', message));
+        log('INFO', message);
     },
     // Relevant to debugging, should not be used in production
     debug(message) {
-        log(getLogString('DEBUG', message));
+        log('DEBUG', message);
     },
     // Something happened to influence the outcome of a command, but there was still a succesfull result to show to the user
     warn(message) {
-        log(getLogString('WARNING', message));
+        log('WARNING', message);
     },
     // A non-fatal error occured, the bot is still online, but there was no output to show to the user
     error(message) {
-        log(getLogString('ERROR', message));
+        log('ERROR', message);
     },
     // A fatal error occured, the bot is no longer online, and the user did not get a response to their command
     fatal(message) {
-        log(getLogString('FATAL', message));
+        log('FATAL', message);
     }
 }
